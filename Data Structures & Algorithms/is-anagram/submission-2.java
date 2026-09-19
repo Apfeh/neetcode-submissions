88c1bb94d4e332bb09e0041d53d0a2eff25a94c7
Apfeh Rams <140980216+Apfeh@users.noreply.github.com>
2026-09-20 00:16:49 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        
+        if(t.length() != s.length()){return false;}
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            sMap.put(c, sMap.getOrDefault(c,0) + 1);
+        }
+        
+
+        for(int x = 0; x < t.length(); x++){
+            char c = t.charAt(x);
+            if(sMap.get(c) == null){
+                return false;
+            }
+            int count = sMap.get(c);
+            if(count == 1){
+                sMap.remove(c);
+            }else{
+                sMap.put(c, count - 1);
+            }
+        }
+        return sMap.isEmpty();
+    }
+}
